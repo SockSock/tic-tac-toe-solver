bde45260dfbd7a566d47311b462cce27c7e730bb
socksock <socksock@DESKTOP-85R8IA6>
2024-02-29 16:47:22 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tic-tac-toe-robotics-project
+Tic Tac Toe Robotics Project for the 1st Year Programming Module Project
---
+++ src/LowBattery.java
@@ -0,0 +1,30 @@
+import lejos.hardware.Battery;
+import lejos.hardware.Sound;
+import lejos.hardware.lcd.LCD;
+import lejos.robotics.subsumption.Behavior;
+
+public class LowBattery implements Behavior {
+	public LowBattery() {
+
+	}
+	
+	@Override
+	public boolean takeControl() {
+		if (Battery.getVoltage() <= 6.5) {
+			LCD.drawString("Battery is low bro, you gotta recharge me fr fr", 0, 2);
+			return true;
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void action() {
+
+	}
+
+	@Override
+	public void suppress() {
+
+	}
+}
